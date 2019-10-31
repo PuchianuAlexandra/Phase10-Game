09dcea0f61be0158d7b54bf8d1955b52093281b4
AlexandraPuchianu <puchianu_alexandra@yahoo.com>
2019-10-31 15:42:57 +0200

    Here we implements functions for get and set in Player class. Also we have overloaded the operator << for writing a player attributes.

--- Phase 10/Player.cpp
+++ Phase 10/Player.cpp
@@ -9,3 +9,38 @@ Player::~Player()
 {
 
 }
+
+std::string Player::GetName() const
+{
+	return Player::m_name;
+}
+
+uint8_t Player::GetScore() const
+{
+	return m_score;
+}
+
+void Player::setScore(uint8_t score)
+{
+	m_score = score;
+}
+
+std::ostream& operator<<(std::ostream& out, const Player& player)
+{
+	out << player.m_name << " have " << player.m_score << " points.\n";
+	out << player.m_name << "'s cards: ";
+	for (auto obj : player.m_handCards)
+	{
+		out << obj.GetValue() << " ";
+		//out << obj.Card::GetColor() << "\n";
+	}
+
+	//out << "Discaded cards: \n";
+	//for (auto obj : player.m_decartedCards)
+	//{
+	//	out << obj. << " ";
+	//	//out << obj.Card::GetColor() << "\n";
+	//}
+
+	return out;
+}
--- Phase 10/Player.h
+++ Phase 10/Player.h
@@ -10,13 +10,19 @@ public:
 
 	Player(std::string name);
 	~Player();
+	std::string GetName() const;
+	uint8_t GetScore() const;
+	void setScore(uint8_t score);
+	friend std::ostream& operator<<(std::ostream& out, const Player& player);
 
-private:
-	std::string m_name;
-	uint8_t m_score=0;
+
+public:
 	std::vector<bool>m_phase = { 0 };
 	std::vector<Card>m_handCards;
 	std::vector<std::vector<Card>>m_decartedCards;
 
+private:
+	std::string m_name;
+	uint8_t m_score=0;
 };
 
