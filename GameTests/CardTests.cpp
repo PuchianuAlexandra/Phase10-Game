ba97bbe1f9824ba83c5444c15bf3e7e39c014ecd
Parpalea Bianca <parpaleabianca@yahoo.com>
2019-12-13 15:13:59 +0200

    We created tests for PrintCard, constructor of Player class, GetCurrentPhase, DropCard and CountScore.

---
+++ GameTests/CardTests.cpp
@@ -0,0 +1,31 @@
+#include "CppUnitTest.h"
+#include "pch.h"
+#include "../Phase 10/Card.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Phase10Tests
+{
+	TEST_CLASS(CardTests)
+	{
+	public:
+
+		TEST_METHOD(Constructor)
+		{
+			Card card(Color::BLUE, Status::NINE, Place::HAND);
+
+			Assert::IsTrue(Color::BLUE == card.GetColor());
+			Assert::IsTrue(Status::NINE == card.GetStatus());
+			Assert::IsTrue(Place::HAND == card.GetPlace());
+		}
+
+		TEST_METHOD(Print)
+		{
+			Card card(Color::BLUE, Status::NINE, Place::HAND);
+
+			std::stringstream stream;
+			stream << card;
+			Assert::AreEqual(std::string("Blue 9 "), stream.str(), L"If you see this message, piece is not printed correctly");
+		}
+	};
+}
